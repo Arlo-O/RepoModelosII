1942fd477bea23a0703dabe2762eeee858c1f2c6
poveda27 <116394608+poveda27@users.noreply.github.com>
2024-09-16 17:28:41 -0500

    Update mayorRectangulo.hs

--- Haskell_Ejecicios/mayorRectangulo.hs
+++ Haskell_Ejecicios/mayorRectangulo.hs
@@ -1,4 +1,4 @@
 mayorRectangulo :: (Integer, Integer) -> (Integer, Integer) -> (Integer, Integer)
-mayorRectangulo r1@(l1, a1) r2@(l2, a2)
+mayorRectangulo r1@(l1, a1) r2@(l2, a2)|l1*a1>= l2*a2=r1| otherwise=r2
 main = do
    print( xor1   (4,6)(3,7))
